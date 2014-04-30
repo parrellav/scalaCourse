492da9f0d2363f9746e5bac2570b9ec5a2dd1da7
vparrella <vincent_parrella@cable.comcast.com>
2014-04-30 08:30:47 -0400

    starting assignment3/week1

--- week1/recfun/src/main/scala/recfun/Main.scala
+++ week1/recfun/src/main/scala/recfun/Main.scala
@@ -3,23 +3,31 @@ import common._
 
 object Main {
   def main(args: Array[String]) {
-        println("Pascal's Triangle")
-        for (row <- 0 to 10) {
-          for (col <- 0 to row)
-            print(pascal(col, row) + " ")
-          println()
-        }
-    println("Exercise 2")
-    checkBalance("(just an) example")
-    checkBalance("I told him (that it’s not (yet) done). (But he wasn’t listening)")
-    checkBalance("())(")
-    checkBalance(":-)")
-    checkBalance("()")
-    checkBalance("(")
-    checkBalance(")")
-    checkBalance("()(())")
+//        println("Pascal's Triangle")
+//        for (row <- 0 to 10) {
+//          for (col <- 0 to row)
+//            print(pascal(col, row) + " ")
+//          println()
+//        }
+//    println("Exercise 2")
+//    checkBalance("(just an) example")
+//    checkBalance("I told him (that it’s not (yet) done). (But he wasn’t listening)")
+//    checkBalance("())(")
+//    checkBalance(":-)")
+//    checkBalance("()")
+//    checkBalance("(")
+//    checkBalance(")")
+//    checkBalance("()(())")
 
-    println()
+    println("Exercise 3")
+
+    printChange(4, List(1,2))
+
+    def printChange(amount: Int, coins: List[Int]) = {
+      println("Amount: " + amount)
+      println("Coins: " + coins)
+      println("Change combinations: " + countChange(amount, coins))
+    }
 
     def checkBalance(s: String) = {
       println(s + (if(balance(s.toList)) " is balanced" else " is not balanced"))
@@ -44,7 +52,7 @@ object Main {
   def balance(chars: List[Char]): Boolean = {
 
     def isBalanced(chars: List[Char], accumulator: Int): Boolean = {
-      if(chars.isEmpty) return accumulator == 0
+      if(chars.isEmpty) accumulator == 0
       else if (chars.head == '(') {
         isBalanced(chars.tail, accumulator+1)
       }
@@ -63,5 +71,13 @@ object Main {
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+
+    def count(money: Int, coins: List[Int], times: Int): Int = {
+      if (coins.isEmpty) return times
+      if(money % coins.head == 0) count(money, coins.tail, times + 1)
+      else times
+    }
+    count(money, coins, 0)
+  }
 }
