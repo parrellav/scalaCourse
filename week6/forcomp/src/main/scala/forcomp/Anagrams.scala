7f0eead145987b48a28174c77ea7b18cc3766cfe
vparrella <vincent_parrella@cable.comcast.com>
2014-06-13 07:55:11 -0400

    continuing anagrams

--- week6/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ week6/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -54,10 +54,10 @@ object Anagrams {
    *    List(('a', 1), ('e', 1), ('t', 1)) -> Seq("ate", "eat", "tea")
    *
    */
-  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = ???
+  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = dictionary.groupBy(wordOccurrences)
 
   /** Returns all the anagrams of a given word. */
-  def wordAnagrams(word: Word): List[Word] = ???
+  def wordAnagrams(word: Word): List[Word] = dictionaryByOccurrences.getOrElse(wordOccurrences(word), Nil)
 
   /** Returns the list of all subsets of the occurrence list.
    *  This includes the occurrence itself, i.e. `List(('k', 1), ('o', 1))`
@@ -81,7 +81,15 @@ object Anagrams {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def combinations(occurrences: Occurrences): List[Occurrences] = occurrences match {
+    case Nil => List(List())
+    case head :: tail =>
+      val combos = combinations(tail)
+      combos ::: (for {
+        combo <- combos
+        i <- 1 to head._2
+      } yield (head._1, i) :: combo )
+  }
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    * 
